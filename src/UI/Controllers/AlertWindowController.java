eecef754fe9dfd5212320ddc932b031a0242335e
Josh Campitelli <joshcampitelli@cmail.carleton.ca>
2018-01-23 00:05:19 -0500

    Added CSS to FXML Alert Window.

--- src/UI/Controllers/AlertWindowController.java
+++ src/UI/Controllers/AlertWindowController.java
@@ -12,6 +12,7 @@ public class AlertWindowController  extends Controller implements Initializable 
     final int ERROR_MESSAGE = 0;
     final int INFO_MESSAGE = 1;
     final int GENERAL_MESSAGE = 2;
+    //todo: redesign fxml window to allow for images based on the state of the controller.
 
     //Attributes
     private int state;
@@ -24,15 +25,13 @@ public class AlertWindowController  extends Controller implements Initializable 
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-
+        titleLbl.setText(title);
+        messageLbl.setText(message);
     }
 
     public void initData(int state, String title, String message) {
-
+        this.state = state;
+        this.title = title;
+        this.message = message;
     }
-
-    /**
-     * Todo: Implement a similar window style to the AWT Java window: constructor accepts
-     * todo: a defined int value (ie. 0 for error, 1 for information, 2 for general message)
-     */
 }
